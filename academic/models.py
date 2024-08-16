7b268a3576dd070e2704250e2c425e2363cdf628
Boaz-Maroko <boazmaroko123@gmail.com>
2024-08-16 15:46:58 +0300

    changed the academics app, made a few changes to the code, check the comments

--- academic/models.py
+++ academic/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import uuid
 
 
 # Create your models here.
@@ -23,150 +22,44 @@ class Course(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
     course_name = models.CharField(max_length=200)
     decription = models.TextField(null=True, blank=True)
-    past_paper = models.CharField(max_length=50, null=True, blank=True)
-    pastPaper1_1 = models.ManyToManyField('PastPaper1_1', blank=True)
-    pastPaper1_2 = models.ManyToManyField('PastPaper1_2', blank=True)
-    pastPaper2_1 = models.ManyToManyField('PastPaper2_1', blank=True)
-    pastPaper2_2 = models.ManyToManyField('PastPaper2_2', blank=True)
-    pastPaper3_1 = models.ManyToManyField('PastPaper3_1', blank=True)
-    pastPaper3_2 = models.ManyToManyField('PastPaper3_2', blank=True)
-    pastPaper4_1 = models.ManyToManyField('PastPaper4_1', blank=True)
-    pastPaper4_2 = models.ManyToManyField('PastPaper4_2', blank=True)
-    notes1_1 = models.ManyToManyField('Notes1_1', blank=True)
-    notes1_2 = models.ManyToManyField('Notes1_2', blank=True)
-    notes2_1 = models.ManyToManyField('Notes2_1', blank=True)
-    notes2_2 = models.ManyToManyField('Notes2_2', blank=True)
-    notes3_1 = models.ManyToManyField('Notes3_1', blank=True)
-    notes3_2 = models.ManyToManyField('Notes3_2', blank=True)
-    notes4_1 = models.ManyToManyField('Notes4_1', blank=True)
-    notes4_2 = models.ManyToManyField('Notes4_2', blank=True)
     
     
     def __str__(self):
         return self.course_name
-    
-    
-class PastPaper1_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
+
+
+# define models to hold units
+
+class CourseUnit(models.Model):
+    course = models.ManyToManyField(Course)
+    unit_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
-    
-class PastPaper1_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
+        return self.unit_name
 
 
-    def __str__(self):
-        return self.name
-    
-class PastPaper2_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
 
-    def __str__(self):
-        return self.name
-    
-class PastPaper2_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
+# let's define a model tha holds past papers
+
+class PastPapers(models.Model):
+    unit = models.ForeignKey(CourseUnit, on_delete=models.CASCADE)
+    name = models.CharField("past paper name", max_length=50)
+    past_paper_file = models.FileField(upload_to="past papers")
+
+    class Meta:
+        verbose_name_plural = "past papers"
 
     def __str__(self):
         return self.name
 
-
-class PastPaper3_1(models.Model):
+# define a model to store the notes
+class Notes(models.Model):
+    unit = models.ForeignKey(CourseUnit, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
+    notes_file = models.FileField(upload_to="notes")
 
     def __str__(self):
         return self.name
-    
-class PastPaper3_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class PastPaper4_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-class PastPaper4_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-    
-       
-class Notes1_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-class Notes1_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-class Notes2_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-class Notes2_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Notes3_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-class Notes3_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Notes4_1(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-    
-class Notes4_2(models.Model):
-    name = models.CharField(max_length=50)
-    link = models.URLField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-
 
 
 
