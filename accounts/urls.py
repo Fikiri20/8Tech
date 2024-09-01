72bfc8190134a65118169f67cad9166dde05d7a3
Boaz-Maroko <boazmaroko123@gmail.com>
2024-09-01 10:27:30 +0300

    added reset password options

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('password_reset/', views.password_reset_request, name='password_reset'),
+    path('reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
+    path('password_reset/done/', views.password_reset_done, name='password_reset_done'),  
+    path('reset/done/', views.password_reset_complete, name='password_reset_complete'), 
 ]
